7b836beae50d7f04cffa66894945a45f84f878da
zhangtj <1771988873@qq.com>
2020-04-11 14:49:13 +0800

    添加numpy数组索引和切片的示例

--- Machine_study/numpy_test.py
+++ Machine_study/numpy_test.py
@@ -19,7 +19,16 @@ import numpy as np
 # print(arr3.dtype)
 
 # print(np.zeros((10, 10, 10)))
-print(np.zeros((3, 5)))
-print(np.ones((4, 6)))
+# print(np.zeros((3, 5)))
+# print(np.ones((4, 6)))
+#
+# print(np.empty((2, 3, 2)))
 
-print(np.empty((2, 3, 2)))
+
+arr5 = np.arange(10)
+arr5[5:8] = 10
+print(arr5)
+
+arr_slice = arr5[5:8].copy()
+arr_slice[:] = 15
+print(arr_slice)
