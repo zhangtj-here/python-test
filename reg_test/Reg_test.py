e507779194de2413f47c7fa3517883b12a339727
zhangtj <1771988873@qq.com>
2020-04-11 08:02:52 +0800

    这则示例的补充

--- reg_test/Reg_test.py
+++ reg_test/Reg_test.py
@@ -33,7 +33,7 @@ import re
 # print(p.match('caat'))
 # print(p.match('ca2t'))
 
-# p = re.compile(r'ca\st') # \s 表示一位字符串，包括a-z,A-Z ?测试不是描述的这样
+# p = re.compile(r'ca\st') # \s 表示一个空格，\S表示非空格
 # print(p.match('caat'))
 # print(p.match('ca1t'))
 
@@ -60,11 +60,25 @@ import re
 # print(year, month, day)
 # print(p.match('18-05-10'))
 
-p = re.compile(r'(\d{1,4})-(\d+)-(\d+)') # () 表示分组
-print(p.search('aa20a18-05-1a0bb').groups()) # serach 表示搜索匹配
+# p = re.compile(r'(\d{1,4})-(\d+)-(\d+)') # () 表示分组
+# print(p.search('aa20a18-05-1a0bb').groups()) # serach 表示搜索匹配
 # print(p.match('2018-05-10').group(1))
 
 
+phone = '123-456-789 # 这是电话号码'
+# p = re.sub('#.*$', '', phone) # sub用于正则匹配替换，类似于js里的replace
+# print(p)
+
+
+# p = re.sub(r'\S', '1', phone) # sub用于正则匹配替换，类似于js里的replace
+# print(p)
+
+# p1 = re.search('(\d+)-(\d+)-(\d+)', 'aa123-456-789bb')
+# print(p1.groups())
+
+p1 = re.findall('\d+', 'aa123-456-789bb')  # findall全局匹配，类似于js正则里面//g
+print(p1)
+# findall()
 # p1 = re.compile('.{3}') #.表示任意字符
 # print(p1.match('a*^'))
 #
