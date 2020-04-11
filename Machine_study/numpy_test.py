182dff5bb82a40a872a8e12e85ca5879c7ee32e2
zhangtj <1771988873@qq.com>
2020-04-11 14:35:39 +0800

    添加numpy对数组和矩阵的一些操作

---
+++ Machine_study/numpy_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# arr = np.array([2, 3, 4])
+#
+# print(arr)
+# print(arr.dtype)
+#
+# arr2 = np.array([1.2, 2.3, 3.4])
+# print(arr2)
+# print(arr2.dtype)
+#
+# print(arr + arr2)
+#
+# print(arr2 * 10)
+
+# data = [[1, 2, 3], [4, 5, 6]]
+# arr3 = np.array(data)
+# print(arr3)
+# print(arr3.dtype)
+
+# print(np.zeros((10, 10, 10)))
+print(np.zeros((3, 5)))
+print(np.ones((4, 6)))
+
+print(np.empty((2, 3, 2)))
